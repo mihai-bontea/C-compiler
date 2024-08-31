5656e66a840d0043ad8a5897e3b944b9f04a2bc9
mihai-bontea <mihaibontea@protonmail.com>
2024-08-31 20:15:02 +0300

    separating the lexer

--- compiler.cpp
+++ compiler.cpp
@@ -5,20 +5,21 @@
 
 #include "Token.hpp"
 
-
-int main(int argc, char **argv) {
-    std::regex all_regex(R"(([a-zA-Z_]\w*\b)|(\b\d+(\.\d+)?\b)|("(\\.|[^\\"])*")|([\+\-\*/%=<>&\|!~^]+)|([;,\(\)\{\}\[\]])|(//.*|/\*[\s\S]*?\*/))");
-
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
+std::vector<Token> get_tokens_from_file(const char* filename)
+{
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
         std::cerr << "Error opening file!" << std::endl;
-        return 1;
+        exit(1);
     }
 
+    std::regex all_regex(R"(([a-zA-Z_]\w*\b)|(\b\d+(\.\d+)?\b)|("(\\.|[^\\"])*")|([\+\-\*/%=<>&\|!~^]+)|([;,\(\)\{\}\[\]])|(//.*|/\*[\s\S]*?\*/))");
     std::string line;
     std::vector<Token> tokens;
 
-    while (std::getline(file, line)) {
+    while (std::getline(file, line))
+    {
         std::string::const_iterator searchStart(line.cbegin());
 
         while (searchStart != line.cend()) {
@@ -37,13 +38,18 @@ int main(int argc, char **argv) {
             searchStart = match.suffix().first;
         }
     }
-
     file.close();
+    return tokens;
+}
 
-    for (const auto& token : tokens) {
+int main(int argc, char **argv)
+{
+    // Stage 1: lexer
+    const auto tokens = get_tokens_from_file(argv[1]);
+    for (const auto& token : tokens)
+    {
         std::cout << token << std::endl;
     }
-    std::cout << tokens.size() << std::endl;
-
+    // Stage 2: building the AST
     return 0;
 }
